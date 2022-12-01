e591b77bdab9d2b42514e2b3a3deef0453bee0b5
Your Name <you@example.com>
2022-12-01 15:58:10 +0100

    Jetzt läuft's

---
+++ ddj_cloud/scrapers/test/README.md
@@ -0,0 +1,5 @@
+# Test
+
+**Contact:** Peter Schneider (peter.schneider@fm.wdr.de)
+
+M1 war das Problem
---
+++ ddj_cloud/scrapers/test/test.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+
+from ddj_cloud.utils.storage import upload_dataframe
+
+
+url = "https://raw.githubusercontent.com/robert-koch-institut/SARS-CoV-2-Nowcasting_und_-R-Schaetzung/main/Nowcast_R_aktuell.csv"
+
+
+def run():
+    # Parse into data frame
+    df = pd.read_csv(url, sep=",", decimal=".", low_memory=False)
+
+    upload_dataframe(df, "test/rki_github_r.csv")
+

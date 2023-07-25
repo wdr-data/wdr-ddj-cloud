fd4b5de35a81ddcfe5b9971d9ef583d4205841f0
Jannes Höke <mail@jhoeke.de>
2023-07-25 23:42:22 +0200

    ✨ Patch datawrapper to support JSON payloads needed for locator maps

---
+++ ddj_cloud/utils/datawrapper.py
@@ -0,0 +1,32 @@
+import json
+
+from datawrapper import Datawrapper as DatawrapperOriginal
+import requests as r
+
+
+class Datawrapper(DatawrapperOriginal):
+    def add_data_json(self, chart_id: str, data: dict) -> r.Response:
+        """Add data to a specified chart as json.
+
+        Parameters
+        ----------
+        chart_id : str
+            ID of chart, table or map to add data to.
+        data : pd.DataFrame
+            A pandas dataframe containing the data to be added.
+
+        Returns
+        -------
+        requests.Response
+            A requests.Response
+        """
+
+        _header = self._auth_header
+        _header["content-type"] = "text/csv"
+        _data = json.dumps(data)
+
+        return r.put(
+            url=f"{self._CHARTS_URL}/{chart_id}/data",
+            headers=_header,
+            data=_data.encode("utf-8"),
+        )

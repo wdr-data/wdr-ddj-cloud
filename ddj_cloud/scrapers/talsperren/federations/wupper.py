38b99edd5e5d64706f74705eef859482e09870fc
Jannes Höke <mail@jhoeke.de>
2023-11-02 19:27:10 +0100

    ♻️ Use AJAX API to get history for Wupperverband

--- ddj_cloud/scrapers/talsperren/federations/wupper.py
+++ ddj_cloud/scrapers/talsperren/federations/wupper.py
@@ -1,59 +1,68 @@
-import re
 import datetime as dt
-from typing import Iterable, Optional
 
 import requests
-import bs4
 
-from ..common import ReservoirRecord, Federation, TZ_BERLIN, skip_errors_and_none
+from ..common import TZ_UTC, ReservoirRecord, Federation
 
 
 class WupperFederation(Federation):
     name = "Wupperverband"
 
-    url = "https://hochwasserportal.wupperverband.de/Talsperren/"
-
     reservoirs = {
         "Bever-Talsperre": {
+            "ajax_id": "SBE$-T",
             "capacity": 23.75,
         },
         "Brucher-Talsperre": {
+            "ajax_id": "SBR$-T",
             "capacity": 3.37,
         },
         "Eschbachtalsperre": {
+            "ajax_id": "SET$-T",
             "capacity": 1.05,
         },
         "Große Dhünn-Talsperre": {
+            "ajax_id": "SHA$-T",
             "capacity": 81.00,
         },
         "Vorsperre Große Dhünn": {
+            "ajax_id": "SVO$-T",
             "capacity": 7.37,
         },
         "Herbringhauser Talsperre": {
+            "ajax_id": "SHT$-T",
             "capacity": 2.90,
         },
         "Kerspe-Talsperre": {
+            "ajax_id": "SKT$-T",
             "capacity": 14.90,
         },
         "Lingese-Talsperre": {
+            "ajax_id": "SLI$-T",
             "capacity": 2.60,
         },
         "Neyetalsperre": {
+            "ajax_id": "SNE$-T",
             "capacity": 5.98,
         },
         "Panzer-Talsperre": {
+            "ajax_id": "SPAZ-T",
             "capacity": 0.19,
         },
         "Ronsdorfer Talsperre": {
+            "ajax_id": "SRO$-T",
             "capacity": 0.12,
         },
         "Schevelinger-Talsperre": {
+            "ajax_id": "SSE$-T",
             "capacity": 0.31,
         },
         "Wupper-Talsperre": {
+            "ajax_id": "SWU$-T",
             "capacity": 25.60,
         },
         "Stausee Beyenburg": {
+            "ajax_id": "SBY$-T",
             "capacity": 0.47,
         },
     }
@@ -77,49 +86,36 @@ class WupperFederation(Federation):
     def __init__(self) -> None:
         super().__init__()
 
-    def _get_html(self) -> str:
-        return requests.get(self.url).text
+    def _build_url(self, ajax_id: str) -> str:
+        return f"https://hochwasserportal.wupperverband.de/?ajax=MapData&id={ajax_id}"
 
-    def _parse_row_div(self, div: bs4.Tag) -> Optional[ReservoirRecord]:
-        name: str = div.find("div", {"data-type": "Name"}).text.strip()  # type: ignore
-        assert name, "No title found"
+    def _get_reservoir_records(self, name: str) -> list[ReservoirRecord]:
+        url = self._build_url(self.reservoirs[name]["ajax_id"])
+        response = requests.get(url).json()
+        content_data = response["Speicherinhalt"]
+        columns: list[str] = content_data["lines"]
+        assert all(
+            [column in columns for column in ["time", "Speicherinhalt"]]
+        ), f"Unexpected column names: '{columns}' does not contain 'time' and 'Speicherinhalt'"
 
-        if name in self.ignore_reservoirs:
-            return None
+        timestamp_idx = columns.index("time")
+        value_idx = columns.index("Speicherinhalt")
 
-        assert name in self.reservoirs, f"Unknown reservoir name: {name}"
-        name_renamed = self.renames.get(name, name)
+        return [
+            ReservoirRecord(
+                self.name,
+                self.renames.get(name, name),
+                dt.datetime.fromtimestamp(row[timestamp_idx], TZ_UTC),
+                self.reservoirs[name]["capacity"],
+                row[value_idx],
+            )
+            for row in content_data["data"]
+            if row[value_idx] is not None
+        ]
 
-        content_div: bs4.Tag = div.find("div", {"data-type": "Speicherinhalt"})  # type: ignore
-        content_span: bs4.Tag = content_div.find("span").find("span")  # type: ignore
-        assert content_span, "No content span found"
-        content_match = re.search(r"([\d\.]+) hm³", content_span.text)
-        assert content_match, "No match found for content"
-        content_mio_m3 = float(content_match.group(1))
-
-        ts_small: bs4.Tag = content_div.find("small", recursive=True)  # type: ignore
-        assert ts_small, "No timestamp small found"
-        assert ts_small.text.strip(), "Timestamp small has no content"
-        ts = dt.datetime.strptime(ts_small.text.strip(), "%d.%m.%y %H:%M").replace(tzinfo=TZ_BERLIN)
-
-        return ReservoirRecord(
-            self.name,
-            name_renamed,
-            ts,
-            self.reservoirs[name]["capacity"],
-            content_mio_m3,
-        )
-
-    def get_data(self, **kwargs) -> Iterable[ReservoirRecord]:
-        html = self._get_html()
-        soup = bs4.BeautifulSoup(html, "lxml")
-
-        coords_div: bs4.Tag = soup.find("div", {"class": "list talsperre"})  # type: ignore
-        assert coords_div, "div with class `list talsperre` not found"
-
-        # return [self._parse_row_div(div) for div in coords_div.children if isinstance(div, bs4.Tag)]
-        row_divs: bs4.ResultSet[bs4.Tag] = coords_div.find_all("div", {"class": "row"}, recursive=False)  # type: ignore
-        return skip_errors_and_none(
-            self._parse_row_div,
-            row_divs,
-        )
+    def get_data(self, **kwargs) -> list[ReservoirRecord]:
+        return [
+            record
+            for name in self.reservoirs.keys()
+            for record in self._get_reservoir_records(name)
+        ]

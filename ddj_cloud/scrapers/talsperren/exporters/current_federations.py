d3ad7ae211df85d3b6e854457dfee3ffa1ba8cc0
Jannes Höke <mail@jhoeke.de>
2024-05-28 18:44:58 +0200

    📊 Add "Alle Talsperren" row

--- ddj_cloud/scrapers/talsperren/exporters/current_federations.py
+++ ddj_cloud/scrapers/talsperren/exporters/current_federations.py
@@ -33,6 +33,23 @@ class CurrentFederationsExporter(Exporter):
             .reset_index()
         )
 
+        # Add "Alle Talsperren" row
+        df_map = pd.concat(
+            [
+                df_map,
+                pd.DataFrame(
+                    [
+                        {
+                            "federation_name": "Alle Talsperren",
+                            "capacity_mio_m3": df_map["capacity_mio_m3"].sum(),
+                            "content_mio_m3": df_map["content_mio_m3"].sum(),
+                        }
+                    ]
+                ),
+            ],
+            ignore_index=True,
+        )
+
         # Add fill percentage
         df_map["fill_percent"] = df_map["content_mio_m3"] / df_map["capacity_mio_m3"] * 100
 

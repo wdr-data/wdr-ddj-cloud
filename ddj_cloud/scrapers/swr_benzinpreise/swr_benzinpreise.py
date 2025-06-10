f924e8d91eb7f7dbf25d7edab53bad53e1fce115
Jannes Höke <mail@jhoeke.de>
2025-06-10 12:56:46 +0200

    ✨ Get high-resolution data for SWR Benzinpreise

--- ddj_cloud/scrapers/swr_benzinpreise/swr_benzinpreise.py
+++ ddj_cloud/scrapers/swr_benzinpreise/swr_benzinpreise.py
@@ -8,7 +8,7 @@ import pandas as pd
 from google.cloud import bigquery
 
 from ddj_cloud.utils import bigquery as bigquery_utils
-from ddj_cloud.utils.date_and_time import local_today
+from ddj_cloud.utils.date_and_time import local_now, local_today
 from ddj_cloud.utils.storage import upload_dataframe
 
 PROJECT = "swr-datalab-prod"
@@ -63,6 +63,53 @@ def load_tageswerte(client: bigquery.Client):
     )
 
 
+def load_aufloesung(client: bigquery.Client):
+    query = "SELECT * FROM `@table_name` WHERE ags = @ags AND datenstand BETWEEN @datenstand_start AND @datenstand_end ORDER BY datenstand DESC, type ASC"
+    query = bigquery_utils.insert_table_name(query, TABLE_AUFLOESUNG, "@table_name")
+    now = local_now()
+    start = now - dt.timedelta(hours=48)
+    end = now + dt.timedelta(days=1)
+
+    job_config = bigquery.QueryJobConfig(
+        default_dataset=DATASET,
+        query_parameters=[
+            bigquery.ScalarQueryParameter("ags", "STRING", "05"),
+            bigquery.ScalarQueryParameter("datenstand_start", "TIMESTAMP", start),
+            bigquery.ScalarQueryParameter("datenstand_end", "TIMESTAMP", end),
+        ],
+    )
+
+    def df_cleaner(df: pd.DataFrame) -> pd.DataFrame:
+        df.drop(
+            columns=[
+                "auflsg_mittel_min_10",
+                "auflsg_mittel_min_25",
+                "auflsg_mittel_min_50",
+                "auflsg_mittel_min_75",
+                "auflsg_mittel_max_10",
+                "auflsg_mittel_max_25",
+                "auflsg_mittel_max_50",
+                "auflsg_mittel_max_75",
+                "n_unique_dates",
+                "n_preise",
+                "ags",
+                "meldedatum",
+            ],
+            inplace=True,
+        )
+        df["type"] = df["type"].map({1: "octane95", 2: "e10", 3: "diesel"})
+        df.reset_index(drop=True)
+
+        return df.replace({np.nan: None})
+
+    yield from bigquery_utils.iter_results(
+        client,
+        query,
+        job_config,
+        df_cleaner,
+    )
+
+
 def expand_column(
     df: pd.DataFrame,
     target_column: str,
@@ -94,6 +141,7 @@ def run():
 
     # Load data
     df_tageswerte = pd.DataFrame(load_tageswerte(bigquery_client))  # Full history since 2022-01-01
+    upload_dataframe(df_tageswerte, "swr_benzinpreise/history_original.csv")
 
     # Latest data in some kind of dashboard format
     df_latest = df_tageswerte[df_tageswerte["day"] == df_tageswerte["day"].max()].copy()
@@ -105,6 +153,8 @@ def run():
         },
         inplace=True,
     )
+
+    upload_dataframe(df_latest, "swr_benzinpreise/latest.csv")
 
     # Expand data
     tageswerte_expanded_dfs = [
@@ -121,13 +171,30 @@ def run():
         ),
         tageswerte_expanded_dfs,
     )
+    upload_dataframe(df_tageswerte_expanded, "swr_benzinpreise/history.csv")
 
     df_tageswerte_30_days_expanded = df_tageswerte_expanded[
         df_tageswerte_expanded["day"] >= (local_today() - dt.timedelta(days=30))
     ]
-
-    # Upload to S3
-    upload_dataframe(df_tageswerte_expanded, "swr_benzinpreise/history.csv")
     upload_dataframe(df_tageswerte_30_days_expanded, "swr_benzinpreise/history_30_days.csv")
-    upload_dataframe(df_tageswerte, "swr_benzinpreise/history_original.csv")
-    upload_dataframe(df_latest, "swr_benzinpreise/latest.csv")
+
+    # Load data
+    df_aufloesung = pd.DataFrame(load_aufloesung(bigquery_client))
+
+    # Expand data
+    aufloesung_expanded_dfs = [
+        expand_column(df_aufloesung, "type", fuel_type, ["datenstand"])
+        for fuel_type in ["octane95", "e10", "diesel"]
+    ]
+
+    df_aufloesung_expanded = reduce(
+        partial(
+            pd.merge,
+            how="outer",
+            on="datenstand",
+            copy=False,
+            validate="one_to_one",
+        ),
+        aufloesung_expanded_dfs,
+    )
+    upload_dataframe(df_aufloesung_expanded, "swr_benzinpreise/history_48_hours.csv")
